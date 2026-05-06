8627aa98b02847c034383e672f3bcbae26636d97
onli00312-byte <onli00312@gmail.com>
2026-05-06 13:47:52 +0400

    block devtool

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/main.css'
-
+import setupDisableDevtools from './utils/disable-devtools'
 createApp(App).mount('#app')
+
+setupDisableDevtools({
+    enableDetector: true,
+})
